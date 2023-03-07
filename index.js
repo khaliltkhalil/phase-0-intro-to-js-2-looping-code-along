2087c666c38ad65d475cbffbee42f5264fafbc95
Khalil Khalil <khalil.t.khalil@live.com>
2023-03-06 20:57:55 -0600

    writes the functions

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, occation) {
+    const cards = [];
+    for(let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${occation} gift!`);
+    }
+    return cards;
+}
+
+function countDown( num ) {
+    while(num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
